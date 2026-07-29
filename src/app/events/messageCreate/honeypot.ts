678d846d10509940f956aab1e140d96c207b3e7d
qwrtsdev <potatoloadzch@gmail.com>
2026-07-29 20:39:05 +0700

    feat: added honeypot event

--- src/app/events/messageCreate/honeypot.ts
+++ src/app/events/messageCreate/honeypot.ts
@@ -1,23 +1,17 @@
 import type { EventHandler } from 'commandkit';
-import { Logger } from 'commandkit/logger';
 import config from '@/config';
 import { softErrorHandling } from '@/utils/softError';
 
 const handler: EventHandler<'messageCreate'> = softErrorHandling('event:messageCreate/honeypot', async (message) => {
   if (message.author.bot) return;
-  if (!config.honeypot_channel) return;
   if (message.channelId !== config.honeypot_channel) return;
 
+  message.deletable && await message.delete();
+
   if (message.member?.permissions.has('Administrator')) return;
+  const target = message.member;
 
-  const member = message.member;
-  if (!member) {
-    Logger.warn('Could not ban honeypot user because guild member data is unavailable.');
-    return;
-  }
-
-  await member.ban({ reason: 'honeypot' });
-  Logger.info(`User ${message.author.id} banned via honeypot.`);
+  await target?.ban({ reason: 'honeypot' });
 });
 
 export default handler;
